17817d07fb417086ca18027ac9cad5a14ea1c60e
fabon <syobon.hinata.public@gmail.com>
2023-07-09 06:56:47 +0900

    Create basic axum project

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use axum::{
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
